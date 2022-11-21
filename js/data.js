70868c28f9e8ee2a2a6d1a35245644acb2c0ebe9
Mateus Gomes <matewsgomes@outlook.com>
2022-11-21 12:43:19 -0300

    adicionado mais conteudo de musicas

--- js/data.js
+++ js/data.js
@@ -1,10 +1,137 @@
 export default [
+    
+    {
+        title:  "Crush Us All",
+        artist: "Seaweed",
+        cover:  "seaweed.jpg",
+        file:   "Crush Us All.mp3"   
+    },
+    
+    {
+        title:  "I Miss You",
+        artist: "Blink 182",
+        cover:  "blink 182.jpg",
+        file:   "I Miss You.mp3"   
+    },
 
     {
-        title: "Beautiful James",
+        title:  "Mr. Brightside",
+        artist: "The Killers",
+        cover:  "mr brigthside.jpg",
+        file:   "Mr. Brightside.mp3"   
+    },
+
+    {
+        title:  "Song 2",
+        artist: "Blur",
+        cover:  "girls and boys.jpg",
+        file:   "Song 2.mp3"   
+    },
+
+
+    {
+        title:  "Buddy Holly",
+        artist: "Weezer",
+        cover:  "buddy holly.jpg",
+        file:   "Buddy Holly.mp3"   
+    },
+
+    {
+        title:  "The Middle",
+        artist: "Jimmy Eat World",
+        cover:  "the middle.jpg",
+        file:   "The Middle.mp3"   
+    },
+
+    {
+        title:  "Everlong",
+        artist: "Foo Fighters",
+        cover:  "foo figthers.jpg",
+        file:   "Everlong.mp3"   
+    },
+    
+    {
+        title:  "Pork And Beans",
+        artist: "Weezer",
+        cover:  "weezer.jpg",
+        file:   "Pork And Beans.mp3"   
+    },
+
+    {
+        title:  "Animal I Have Become",
+        artist: "Three Days Grace",
+        cover:  "Three Days Grace.jpg",
+        file:   "Animal I Have Become.mp3"   
+    },
+
+    {
+        title:  "Sex On Fire",
+        artist: "Kings of Leon",
+        cover:  "sex on fire.jpg",
+        file:   "Sex On Fire.mp3"   
+    },
+
+    {
+        title:  "Digital Bath",
+        artist: "Deftones",
+        cover:  "deftones.png",
+        file:   "Digital Bath.mp3"   
+    },
+
+    {
+        title:  "Resistance",
+        artist: "Muse",
+        cover:  "resistence.jpg",
+        file:   "Resistance.mp3"   
+    },
+
+    {
+        title:  "Girls and Boys",
+        artist: "Blur",
+        cover:  "girls and boys.jpg",
+        file:   "Girls and Boys.mp3"   
+    },
+
+    {
+        title:  "Run For Cover",
+        artist: "The Killers",
+        cover:  "run for cover.jpg",
+        file:   "Run For Cover.mp3"   
+    },
+
+    {
+        title:  "Time is Running Out",
+        artist: "Muse",
+        cover:  "time is running out.jpg",
+        file:   "Time is Running Out.mp3"   
+    },
+
+    {
+        title:  "Special K",
+        artist: "Placebo",
+        cover:  "placebo Special K.jpg",
+        file:   "Special K.mp3"   
+    },
+
+    {
+        title: "Uprising",
+        artist: "Muse",
+        cover:  "resistence.jpg",
+        file: "Uprising.mp3"   
+    },
+
+    {
+        title:  "Read My Mind",
+        artist: "The Killers",
+        cover:  "Killer ready my mind.jpg",
+        file:   "Read My Mind.mp3"   
+    },
+
+    {
+        title:  "Beautiful James",
         artist: "Placebo",
         cover:  "placebo never let me go.jpg",
-        file: "Beautiful James.mp3"   
+        file:   "Beautiful James.mp3"   
     },
 
     {
@@ -15,116 +142,116 @@ export default [
     },
 
     {
-        title: "Drag",
+        title:  "Drag",
         artist: "Placebo",
         cover:  "placebo meds.jpg",
-        file: "Drag.mp3"   
+        file:   "Drag.mp3"   
     },
 
     {
-        title: "You Found Me",
+        title:  "You Found Me",
         artist: "The Fray",
         cover:  "The Fray.jpg",
-        file: "You Found Me.mp3"  
+        file:   "You Found Me.mp3"  
     },
 
     {
-        title: "Whatever It Takes",
+        title:  "Whatever It Takes",
         artist: "Lifehouse",
         cover:  "lifehouse2.jpg",
-        file: "Whatever It Takes.mp3"   
+        file:   "Whatever It Takes.mp3"   
     },
 
     {
-        title: "The Bitter End",
+        title:  "The Bitter End",
         artist: "Placebo",
         cover:  "placebo sleeping with ghosts.jpg",
-        file: "The Bitter End.mp3"   
+        file:   "The Bitter End.mp3"   
     },
 
     {
-        title: "Come Back Down",
+        title:  "Come Back Down",
         artist: "Lifehouse",
         cover:  "lifehouse.jpg",
-        file: "Come Back Down.mp3"
+        file:   "Come Back Down.mp3"
     },
 
     {
-        title: "Angels Or Devils",
+        title:  "Angels Or Devils",
         artist: "Dishwalla",
         cover:  "dishwalla.jpg",
-        file: "Angels Or Devils.mp3"   
+        file:   "Angels Or Devils.mp3"   
     },
 
     {
-        title: "Better Luck Next Time",
+        title:  "Better Luck Next Time",
         artist: "Lifehouse",
         cover:  "lifehouse.jpg",
-        file: "Better Luck Next Time.mp3"
+        file:   "Better Luck Next Time.mp3"
     },
 
     {
-        title: "Blue Sky",
+        title:  "Blue Sky",
         artist: "Hale",
         cover:  "Hale.jpg",
-        file: "Blue Sky.mp3"   
+        file:   "Blue Sky.mp3"   
     },
 
     {
-        title: "Blind",
+        title:  "Blind",
         artist: "Lifehouse",
         cover:  "lifehouse.jpg",
-        file: "Blind.mp3"   
+        file:   "Blind.mp3"   
     },
 
     {
-        title: "Breathing",
+        title:  "Breathing",
         artist: "Lifehouse",
         cover:  "lifehouse1.jpg",
-        file: "Breathing.mp3"   
+        file:   "Breathing.mp3"   
     },
 
     {
-        title: "Broken",
+        title:  "Broken",
         artist: "Lifehouse",
         cover:  "lifehouse2.jpg",
-        file: "Broken.mp3"   
+        file:   "Broken.mp3"   
     },
 
     {
-        title: "Somewhere in the Middle",
+        title:  "Somewhere in the Middle",
         artist: "Dishwalla",
         cover:  "dishwalla.jpg",
-        file: "Somewhere in the Middle.mp3"   
+        file:   "Somewhere in the Middle.mp3"   
     },
 
     {
-        title: "Come Back Down",
+        title:  "Come Back Down",
         artist: "Lifehouse",
         cover:  "lifehouse.jpg",
-        file: "Come Back Down.mp3"   
+        file:   "Come Back Down.mp3"   
     },
 
     {
-        title: "The Day You Said Goodnight",
+        title:  "The Day You Said Goodnight",
         artist: "Hale",
         cover:  "Hale.jpg",
-        file: "The Day You Said Goodnight.mp3"   
+        file:   "The Day You Said Goodnight.mp3"   
     },
 
     {
-        title: "You And Me",
+        title:  "You And Me",
         artist: "Lifehouse",
         cover:  "lifehouse.jpg",
-        file: "You And Me.mp3" 
+        file:   "You And Me.mp3" 
     },
 
     
     {
-        title: "Every Little Thing",
+        title:  "Every Little Thing",
         artist: "Lifehouse",
         cover:  "lifehouse.jpg",
-        file: "Every Little Thing.mp3"   
+        file:   "Every Little Thing.mp3"   
     },
 
 ];
